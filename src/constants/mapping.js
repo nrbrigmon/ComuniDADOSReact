e670618ac10ad79fde12d0f2d4fcaf5ef1d026a5
nrbrigmon <nrbrigmon@gmail.com>
2025-01-18 13:33:36 -0600

    tile server update

--- src/constants/mapping.js
+++ src/constants/mapping.js
@@ -30,7 +30,7 @@ export const BASE_MAP_OPTIONS = function(_mapLayers) {
     <LayersControl.BaseLayer name="OSM B&W" checked={0 === baseMapSelection}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}.png"
+        url="https://{s}.tile-cyclosm.openstreetmap.fr/[cyclosm|cyclosm-lite]/{z}/{x}/{y}.png"
 				opacity={baseMapOpacity}
       />
     </LayersControl.BaseLayer>
